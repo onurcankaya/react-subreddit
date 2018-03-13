e897f05565755932cc7cfe6a9bb887e38ebabe25
onurcankaya <onur.eren.cankaya@gmail.com>
2018-03-13 07:51:13 +0100

    added snapshot tests for Search and Button components

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
-import App from './App'
+import App, { Search, Button, Table } from './App'
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -11,6 +11,32 @@ describe('App', () => {
 
   test('has a valid snapshot', () => {
     const component = renderer.create(<App />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('Search', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Search>Search</Search>, div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Search>Search</Search>)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
+
+describe('Button', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Button>Search input</Button>, div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Button>Search input</Button>)
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
