090f2a02cdd32ae3b7951d1fee23dae35bfef509
onurcankaya <onur.eren.cankaya@gmail.com>
2018-03-15 08:57:32 +0100

    added a unit test to Button component with Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon'
 import App, { Search, Button, Table } from './App'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -32,12 +37,19 @@ describe('Search', () => {
 describe('Button', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Button>Search input</Button>, div)
+    ReactDOM.render(<Button className=".button-inline">Dismiss</Button>, div)
   })
 
   test('has a valid snapshot', () => {
-    const component = renderer.create(<Button>Search input</Button>)
+    const component = renderer.create(<Button>Dismiss</Button>)
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy()
+    const wrapper = shallow(<Button onClick={onButtonClick} />)
+    wrapper.find('button').simulate('click')
+    expect(onButtonClick.calledOnce).toEqual(true)
   })
 })
